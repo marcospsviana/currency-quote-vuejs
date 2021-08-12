cc1e29b4de6a447dc29b7ccbf92540611824349a
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-08-11 22:53:06 -0300

    modificado versao vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+const app = createApp({
+  // root instance definition
+});
+
+app.mount("#app");
